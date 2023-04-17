9f7f33c198a3193042088bf1804f94f81a5db62b
Alex Christian <alexchristianqr@gmail.com>
2023-04-17 18:51:47 -0500

    [added] semana cuatro archivo

---
+++ src/com/alexquispe/utp/SemanaCuatro.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.alexquispe.utp;
+
+/**
+ *
+ * @author LAB-USR-AQ265-A0303
+ */
+public class SemanaCuatro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String cad1 = "Alex";
+        String cad2 = "Deysi";
+        
+        System.out.println(!cad1.equals(cad2));
+    }
+    
+}
