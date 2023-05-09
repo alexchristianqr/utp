a6e1360e10908b629b5756c58dca1dcbded56dac
Alex Christian <alexchristianqr@gmail.com>
2023-05-09 17:03:38 -0500

    [updated] ejercicios 2,3 y4

--- src/com/alexquispe/utp/SemanaSiete.java
+++ src/com/alexquispe/utp/SemanaSiete.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class SemanaSiete {
     public static void main(String[] args) {
-        ejercicio01();
+        ejercicio03();
     }
 
     public static void ejercicio01() {
@@ -29,6 +29,89 @@ public class SemanaSiete {
     }
 
     public static void ejercicio02() {
+        // D
+        int contador = 0, hasta, acumulador = 0;
 
+        // I
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el maximo numero:");
+        hasta = scanner.nextInt();
+
+        // P
+        do {
+            // Al ser diferente de CERO es impar
+            boolean esImpar = (contador % 2) != 0;
+
+            if (esImpar) {
+                System.out.println("El numero impar es: " + contador);
+                // Sumar números impares
+                acumulador = acumulador + contador;
+            }
+
+            contador++;
+        } while (contador <= hasta);
+
+        // S
+        System.out.println("La suma de la serie es: " + acumulador);
+    }
+
+    public static void ejercicio03() {
+        // D
+        int acumulador = 0, cociente, residuo;
+
+        // I
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese un numero para sumar sus digitos:");
+        cociente = scanner.nextInt(); // 75231
+
+        // PS
+        do {
+            residuo = cociente % 10;
+            // Evaluar 2da vez para no perder el valor inicial del cociente
+            cociente = cociente / 10;
+
+            System.out.println("El cociente es: " + cociente + " y su residuo es: " + residuo);
+
+            // Sumar números
+            acumulador = acumulador + residuo;
+        } while (cociente > 0);
+
+        System.out.println("La suma de los digitos es: " + acumulador);
+    }
+
+    public static void ejercicio04() {
+        // D
+        int acumPares = 0, acumImpares = 0, cociente, residuo;
+
+        // I
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese un numero para sumar sus digitos:");
+        cociente = scanner.nextInt(); // 665510
+
+        // P
+        do {
+            residuo = cociente % 10;
+            // Evaluar 2da vez para no perder el valor inicial del cociente
+            cociente = cociente / 10;
+
+            boolean esImpar = (residuo % 2) != 0;// Al ser diferente de CERO es impar
+            boolean esPar = (residuo % 2) == 0;// Al ser igual a CERO es par
+
+            if (esPar) {
+                System.out.println("El numero par es: " + residuo);
+                // Sumar números pares
+                acumPares = acumPares + residuo;
+            }
+
+            if (esImpar) {
+                System.out.println("El numero impar es: " + residuo);
+                // Sumar números impares
+                acumImpares = acumImpares + residuo;
+            }
+
+        } while (cociente > 0);
+
+        System.out.println("La suma de los digitos pares es: " + acumPares);
+        System.out.println("La suma de los digitos impares es: " + acumImpares);
     }
 }
