11ae1fa28476c72ecea6b6fb8705045cf9b388d1
Alex Christian <alexchristianqr@gmail.com>
2023-06-06 13:20:37 -0500

    [updated] matriz bidimensional y vector

--- src/com/alexquispe/utp/Semana11.java
+++ src/com/alexquispe/utp/Semana11.java
@@ -1,11 +1,12 @@
 package com.alexquispe.utp;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Semana11 {
 
     public static void main(String[] args) {
-        ejercicio01();
+        ejercicio04();
     }
 
     public static void ejercicio01() {
@@ -77,15 +78,20 @@ public class Semana11 {
 
         matriz = new int[nfilas][ncolumnas];
 
-        // Cargar arreglo bidimensional
+        // Recorrer las filas
         for (int i = 0; i < matriz.length; i++) {
-            System.out.println("EL tamano de i=" + matriz[i].length);
-            for (int j = 0; j < matriz[i].length; j++) {
 
-                System.out.print("Ingresa dato para la matriz[" + i + "][" + j + "]: ");
-                matriz[i][j] = dato.nextInt();
+            // Obtener el tamaño de la fila[0]
+            int totalFilas = matriz[0].length;
+
+            // Recorrer las columnas
+            for (int j = 0; j < totalFilas; j++) {
+
+                // System.out.print("Ingresa dato para la matriz[" + i + "][" + j + "]: ");
+                // matriz[i][j] = dato.nextInt();
+
                 // (Math.randon) * (Max - Min) + min
-                //matriz[i][j] = (int) (Math.random() * (20 - 1) + 1);
+                matriz[i][j] = (int) (Math.random() * (20 - 1) + 1);
 
                 if ((matriz[i][j] % 2) == 0) {
                     totalPares++;
@@ -101,16 +107,79 @@ public class Semana11 {
 
         // Pintar arreglo bidimensional
         for (int i = 0; i < matriz.length; i++) {
-            // System.out.print("EL tamano de matriz[i] es: " + matriz[i].length);
             for (int j = 0; j < matriz[i].length; j++) {
                 System.out.print("f[" + i + "]c[" + j + "] = " + matriz[i][j] + "\t");
             }
             System.out.println("\n");
-
         }
 
         System.out.println("El total de pares es: " + totalPares);
         System.out.println("El total de impares es: " + totalImpares);
+    }
 
+    public static void ejercicio04() {
+        int[][] matriz;
+        int[] vectorImpares = new int[0];
+        int nfilas, ncolumnas, totalPares = 0, totalImpares = 0;
+        Scanner dato = new Scanner(System.in);
+
+        System.out.print("Ingresa N filas: ");
+        nfilas = dato.nextInt();
+
+        System.out.print("Ingresa N columnas: ");
+        ncolumnas = dato.nextInt();
+
+        matriz = new int[nfilas][ncolumnas];
+        vectorImpares = new int[nfilas * ncolumnas];
+
+        // Recorrer fila
+        for (int i = 0; i < matriz.length; i++) {
+
+            // Obtener el tamaño de la fila[0]
+            int totalFilas = matriz[0].length;
+
+            // Recorrer columna
+            for (int j = 0; j < totalFilas; j++) {
+
+                // (Math.randon) * (Max - Min) + min
+                matriz[i][j] = (int) (Math.random() * (20 - 1) + 1);
+
+                if ((matriz[i][j] % 2) == 0) {
+                    totalPares++;
+                } else {
+                    if ((matriz[i][j] % 2) == 1) {
+//                        vectorImpares = Arrays.copyOf(vectorImpares, totalImpares);
+                        vectorImpares[totalImpares] = matriz[i][j];// Guardar numero impar en un vector
+                        totalImpares++;
+                    }
+                }
+            }
+        }
+
+
+        System.out.print("\n");
+
+        // Pintar matriz bidimensional
+        System.out.println("La matriz bidimensional es: ");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print("f[" + i + "]c[" + j + "] = " + matriz[i][j] + "\t");
+            }
+            System.out.print("\n");
+        }
+        System.out.print("\n");
+
+        // Redimensionar vector
+        vectorImpares = Arrays.copyOf(vectorImpares, totalImpares);
+
+        // Pintar vector
+        System.out.println("El vector es: ");
+        for (int i = 0; i < vectorImpares.length; i++) {
+            System.out.print("f[" + i + "] = " + vectorImpares[i] + "\t");
+        }
+        System.out.println("\n");
+
+        System.out.println("El total de pares es: " + totalPares);
+        System.out.println("El total de impares es: " + totalImpares);
     }
 }
