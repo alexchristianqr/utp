4e7473bc6303bc7d6580f2b419cdc6f8df12673d
Alex Christian <alexchristianqr@gmail.com>
2023-06-05 19:24:59 -0500

    Update Semana11.java

--- src/com/alexquispe/utp/Semana11.java
+++ src/com/alexquispe/utp/Semana11.java
@@ -45,7 +45,8 @@ public static void ejercicio02() {
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
 
-                matriz[i][j] = (int) (Math.random() * 20 + 0);
+                // (Math.randon) * (Max - Min) + min
+                matriz[i][j] = (int) (Math.random() * (20 - 1) +1);
 
             }
 
