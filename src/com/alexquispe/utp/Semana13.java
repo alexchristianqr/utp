21f6060bc2f847bf09392c23bb3b8b854df4ab60
Alex Christian <alexchristianqr@gmail.com>
2023-06-19 19:24:35 -0500

    Update Semana13.java

--- src/com/alexquispe/utp/Semana13.java
+++ src/com/alexquispe/utp/Semana13.java
@@ -8,8 +8,7 @@ package com.alexquispe.utp;
  *
  * @author LAB-USR-AQ265-A0303
  */
-
-class Persona{
+class Persona {
 
     public String getNombre() {
         return nombre;
@@ -50,39 +49,77 @@ class Persona{
     public void setEdad(String edad) {
         this.edad = edad;
     }
-    
+
     public String nombre;
     public String apellidoMaterno;
     public String apellidoPaterno;
     public String sexo;
-    public String edad; 
-    
-    void comer(){
+    public String edad;
+
+    void comer() {
         System.out.println("La persona esta comiendo...");
     }
-    
-    void beber(){
+
+    void beber() {
         System.out.println("La persona esta bebiendo...");
     }
-    
-    void dormir(){
+
+    void dormir() {
         System.out.println("La persona esta durmiendo...");
     }
 }
 
+class Perro {
+
+    String nombre;
+    String colorPelo;
+    int cantidadOjos;
+
+    Perro(String nombre, String colorPelo, int cantidadOjos) {
+        this.nombre = nombre;
+        this.colorPelo = colorPelo;
+        this.cantidadOjos = cantidadOjos;
+    }
+
+    void ladrar() {
+        System.out.println("Ladrando...");
+    }
+
+    void caminar() {
+        System.out.println("Caminando...");
+    }
+
+    void comer() {
+        System.out.println("Comiendo...");
+    }
+}
+
 public class Semana13 {
+
     public static void main(String[] args) {
-        Persona estudiante = new Persona();
-        
+
+        // Crear objeto perro
+        Perro perro = new Perro("Fido", "Marron", 2);
+        System.out.println("----------------------");
+        System.out.println("Información del perro:");
+        System.out.println("----------------------");
+        System.out.println("Nombre: " + perro.nombre);
+        System.out.println("Color: " + perro.colorPelo);
+        System.out.println("Cantidad de ojos: " + perro.cantidadOjos);
+        System.out.print("\n");
+
+        // Crear objeto persona
+        /*Persona estudiante = new Persona();
+
         estudiante.comer();
         estudiante.beber();
         estudiante.dormir();
-        
-        estudiante.setNombre("Juan");
-        estudiante.setApellidoPaterno("Gonzales");
-        estudiante.setApellidoMaterno("Prada");
-        
-        String nombreCompleto = estudiante.getNombre()+" "+estudiante.getApellidoPaterno()+" "+estudiante.getApellidoMaterno();
-        System.out.println("El nombre del estudiente es: "+nombreCompleto);
+
+        estudiante.setNombre("Alex");
+        estudiante.setApellidoPaterno("Quispe");
+        estudiante.setApellidoMaterno("Roque");
+
+        String nombreCompleto = estudiante.getNombre() + " " + estudiante.getApellidoPaterno() + " " + estudiante.getApellidoMaterno();
+        System.out.println("El nombre del estudiente es: " + nombreCompleto);*/
     }
 }
