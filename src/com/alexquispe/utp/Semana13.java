b0b86cf3f673559793a4999b9b48bbd7d01168ec
Alex Christian <alexchristianqr@gmail.com>
2023-06-19 19:46:24 -0500

    Update Semana13.java

--- src/com/alexquispe/utp/Semana13.java
+++ src/com/alexquispe/utp/Semana13.java
@@ -92,6 +92,12 @@ class Perro {
     void comer() {
         System.out.println("Comiendo...");
     }
+
+    public Perro() {
+        this.colorPelo = "Marron";
+    }
+    
+    
 }
 
 public class Semana13 {
@@ -99,7 +105,14 @@ public class Semana13 {
     public static void main(String[] args) {
 
         // Crear objeto perro
-        Perro perro = new Perro("Fido", "Marron", 2);
+        // Perro perro = new Perro("Fido", "Marron", 2);
+        Perro perro = new Perro();
+        perro.nombre = "Fido";
+        perro.cantidadOjos = 2;
+        perro.ladrar();
+        perro.caminar();
+        perro.comer();
+        
         System.out.println("----------------------");
         System.out.println("Información del perro:");
         System.out.println("----------------------");
