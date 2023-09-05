b83b6ce738087ae03a655ce8a51ca8677ad7f2a3
Alex Christian <alexchristianqr@gmail.com>
2023-09-04 23:18:21 -0500

    [updated] query semana 2 sql

--- src/utp/edu/pe/database/Semana02.sql
+++ src/utp/edu/pe/database/Semana02.sql
@@ -1,10 +1,15 @@
+-- # Crear base de datos
+create database ColeccionLibros
+go
+
+-- # Usar base de datos
 use ColeccionLibros
 go
 
+-- # Actualizar base de datos
 -- alter database Coleccionlibros MODIFY NAME =  ColeccionLibros
--- go
 
--- Crear tabla Autor
+-- # Crear tabla Autor
 create table Autor
 (
 Codigo int not null,
@@ -14,28 +19,32 @@ primary key (Codigo)
 )
 go
 
--- alter table Autor drop column paisNacimiento  
--- alter table Autor alter column paisNacimiento varchar(50) 
-alter table Autor add PaisNacimiento varchar(50) 
-go
+-- # Eliminar columna de la tabla
+-- alter table Autor drop column paisNacimiento
 
--- Insertar tabla Autor
+-- # Actualizar columna de la tabla
+-- alter table Autor alter column paisNacimiento varchar(50) 
+
+-- # Agregar columna a la tabla
+-- alter table Autor add PaisNacimiento varchar(50) 
+
+-- # Insertar tabla Autor
 insert into Autor (Codigo, Nombre, PaisNacimiento) values (1,'Cesar Vallejo','Peru')
 insert into Autor (Codigo, Nombre, PaisNacimiento) values (2,'Ricardo Palma','Peru')
 go
 
--- Mostrar tabla Autor
+-- # Mostrar tabla Autor
 select * from Autor
 go
 
--- Actualizar tabla Autor por ID
+-- # Actualizar tabla Autor por ID
 update Autor set PaisNacimiento = 'Peru' where Autor.Codigo = 1
 go
 
--- Eliminar tabla Autor por ID
+-- # Eliminar tabla Autor por ID
 delete Autor where Autor.Codigo = 2;
 
--- Crear tabla Libro
+-- # Crear tabla Libro
 create table Libro
 (
 Codigo int not null,
@@ -50,17 +59,19 @@ foreign key (codigoAutor) references Autor(Codigo)
 )
 go
 
+-- # Eliminar tabla
 -- drop table Libro
 
--- Insertar tabla Libro
+-- # Insertar tabla Libro
 insert into Libro (Codigo, Titulo, CodigoAutor, FechaCompra, Paginas, AnioPublicacion, Descripcion) values (1,'Cien años de soledad',2,GETDATE(),100,1875,'Libro antiguo')
 go
 
--- Mostrar tabla Libro
+-- # Mostrar tabla Libro
 select * from Libro
-select l.*,a.Nombre as 'Nombre Autor' from Libro as l 
+select a.Nombre as 'Nombre Autor',l.Titulo, l.Paginas from Libro as l 
 join Autor as a on a.Codigo = l.CodigoAutor
 go
 
--- Utilidades
--- select GETDATE();
+-- # Utilidades
+select GETDATE()
+go
