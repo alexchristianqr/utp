347c9e4ca0b4ae8f8bca944e6aaa834deb7ab836
Alex Christian <alexchristianqr@gmail.com>
2023-10-18 11:29:39 -0500

    Update Semana10.sql

--- src/utp/edu/pe/database/Semana10.sql
+++ src/utp/edu/pe/database/Semana10.sql
@@ -1,66 +1,86 @@
--- DD/MM/YYYY hh:mm.ss
--- Ejemplo JOIN
+-- Ejemplo de consulta con INNER JOIN y GROUP BY
 select 
-sum(Freight) as 'Total', OrderDate, Freight, o.CustomerID, o.EmployeeID, e.LastName
+sum(Freight) as 'Suma total', 
+OrderDate, 
+Freight, 
+o.CustomerID, 
+o.EmployeeID, 
+e.LastName
 from Orders o
-join Customers c on c.CustomerID = o.CustomerID
-join Employees e on e.EmployeeID = o.EmployeeID
-where year(OrderDate) = 1997 and month(OrderDate) between  7 and 9
-group by OrderDate, Freight, o.CustomerID, o.EmployeeID, e.LastName
+inner join Customers c on c.CustomerID = o.CustomerID
+inner join Employees e on e.EmployeeID = o.EmployeeID
+where year(OrderDate) = 1997 and month(OrderDate) between  1 and 12
+group by OrderDate, Freight, o.CustomerID, o.EmployeeID, e.LastName -- Aplicando agrupamiento
 order by month(OrderDate) desc
 go
 
--- Ejemplo de FUNCTION
-create or alter function dbo.ListarGastosAcumulados
-(
-	@periodo int,
-	@conteo int
-) returns table
-as
-return 
-(
-	select 
-	sum(Freight) as 'Total suma', OrderDate, Freight, o.CustomerID, o.EmployeeID, e.LastName, count(o.CustomerID) as 'Conteo total'
-	from Orders o
-	join Customers c on c.CustomerID = o.CustomerID
-	join Employees e on e.EmployeeID = o.EmployeeID
-	where year(OrderDate) = @periodo -- and month(OrderDate) between  7 and 9
-	group by OrderDate, Freight, o.CustomerID, o.EmployeeID, e.LastName
-	having count(o.CustomerID) >= @conteo
-	-- order by month(OrderDate) desc
-)
-go
-
--- Ejecutar FUNCTION
-select * from dbo.ListarGastosAcumulados(1996,1)
-go
-
--- Ejemplo de FUNCTION
-create or alter function dbo.ListarGastosAcumulados2
-(
-	@periodo int,
-	@conteo int
-) returns table
-as
-return 
-(
-	select 
-	sum(Freight) as 'Total suma', OrderDate, Freight, o.CustomerID, o.EmployeeID, e.LastName, count(o.CustomerID) as 'Conteo total'
-	from Orders o
-	join Customers c on c.CustomerID = o.CustomerID
-	join Employees e on e.EmployeeID = o.EmployeeID
-	where year(OrderDate) = @periodo -- and month(OrderDate) between  7 and 9
-	group by OrderDate, Freight, o.CustomerID, o.EmployeeID, e.LastName
-	having count(o.CustomerID) >= @conteo
-	-- order by month(OrderDate) desc
-)
-go
-
+-- Ejemplo de consulta con INNER JOIN y OVER PARTITION
 select
-sum(Freight) over(partition by  o.CustomerID, o.EmployeeID) as 'Gasto total',
-o.CustomerID,c.CompanyName, o.EmployeeID, e.LastName
+sum(Freight) over(partition by  o.CustomerID, o.EmployeeID) as 'Suma total',
+o.CustomerID,
+c.CompanyName, 
+o.EmployeeID, 
+e.LastName
 from Orders o
 join Customers c on c.CustomerID = o.CustomerID
 join Employees e on e.EmployeeID = o.EmployeeID
-where year(OrderDate) = 1997 and o.CustomerID = 'SAVEA' and o.EmployeeID = 1
+where year(OrderDate) = 1997 and month(OrderDate) between  1 and 12 and o.CustomerID = 'SAVEA' and o.EmployeeID = 1
 go
+
+-- Ejemplo crear o actualizar FUNCTION
+create or alter function dbo.FnListarGastosAcumulados
+(
+	@periodo int,
+	@conteo int
+) returns table
+as
+return 
+(
+	select 
+	sum(Freight) as 'Suma total', 
+	count(o.CustomerID) as 'Conteo total'
+	OrderDate, 
+	Freight, 
+	o.CustomerID, 
+	o.EmployeeID, 
+	e.LastName
+	from Orders o
+	join Customers c on c.CustomerID = o.CustomerID
+	join Employees e on e.EmployeeID = o.EmployeeID
+	where year(OrderDate) = @periodo
+	group by OrderDate, Freight, o.CustomerID, o.EmployeeID, e.LastName
+	having count(o.CustomerID) >= @conteo
+)
+go
+
+-- Ejecutar una FUNCTION
+select * from dbo.FnListarGastosAcumulados(1996, 1)
+go
+
+-- Ejemplo crear o actualizar STORE PROCEDURE
+create or alter function dbo.SpListarGastosAcumulados
+(
+	@periodo int,
+	@conteo int
+)
+as
+begin 
+(
+	select 
+	sum(Freight) as 'Suma total', 
+	count(o.CustomerID) as 'Conteo total',
+	OrderDate, 
+	Freight, 
+	o.CustomerID, 
+	o.EmployeeID, 
+	e.LastName
+	from Orders o
+	join Customers c on c.CustomerID = o.CustomerID
+	join Employees e on e.EmployeeID = o.EmployeeID
+	where year(OrderDate) = @periodo
+	group by OrderDate, Freight, o.CustomerID, o.EmployeeID, e.LastName
+	having count(o.CustomerID) >= @conteo
+)
+end
+go
+
