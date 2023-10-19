9261f643b5cb6c6ec8ed9e665ce06b6450891705
Alex Christian <alexchristianqr@gmail.com>
2023-10-19 15:06:48 -0500

    Update tabla empleado

--- src/utp/edu/pe/database/proyecto-final.sql
+++ src/utp/edu/pe/database/proyecto-final.sql
@@ -1,1 +1,19 @@
+-- # Crear base de datos
+create database db_hotel
+go
+
+  -- # Usar base de datos
+use db_hotel
+go
+
+-- # Crear tabla Empleado
+create table Empleado
+(
+  EmpleadoId int not null,
+  Nombre varchar(100) not null,
+  Apellido varchar(100) not null,
+  Edad varchar(5),
+  primary key (EmpleadoId)
+)
+go
 
