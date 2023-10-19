2633cee0a15d7bf5f0d1965d5c864fd6e6bb5099
Alex Christian <alexchristianqr@gmail.com>
2023-10-19 15:36:47 -0500

    Update proyecto-final.sql
    Agregado persona, empleado y cliente

--- src/utp/edu/pe/database/proyecto-final.sql
+++ src/utp/edu/pe/database/proyecto-final.sql
@@ -2,18 +2,47 @@
 create database db_hotel
 go
 
-  -- # Usar base de datos
+-- # Usar base de datos
 use db_hotel
+go
+
+-- # Crear tabla Persona
+create table Persona
+(
+  PersonaId int not null,
+  Nombre varchar(100) not null,
+  Apellido varchar(100) not null,
+  Edad varchar(5),
+  primary key (PersonaId)
+)
+go
+
+-- # Crear tabla Cliente
+create table Cliente
+(
+  ClienteID int not null,
+  PersonaID int not null,
+  NombreEmpresa varchar(5) not null,
+  TipoDocumento varchar(100) not null,
+  NroDocumento varchar(5) not null,
+  primary key (ClienteID),
+  foreign key (PersonaID) references Persona(PersonaID)
+)
 go
 
 -- # Crear tabla Empleado
 create table Empleado
 (
-  EmpleadoId int not null,
-  Nombre varchar(100) not null,
-  Apellido varchar(100) not null,
-  Edad varchar(5),
-  primary key (EmpleadoId)
+  EmpleadoID int not null,
+  PersonaID int not null,
+  PerfilID decimal not null,
+  Sueldo decimal not null,
+  primary key (EmpleadoID),
+  foreign key (PersonaID) references Persona(PersonaIdD,
+  foreign key (PerfilID) references Perfil(PerfilID)
 )
 go
+
+
+
 
