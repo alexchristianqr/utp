592f7d159da7b71d57c45a8ffbc931224ea847c3
Alex Christian <alexchristianqr@gmail.com>
2023-10-19 20:01:41 -0500

    Update Semana10.sql

--- src/utp/edu/pe/database/Semana10.sql
+++ src/utp/edu/pe/database/Semana10.sql
@@ -90,3 +90,98 @@ go
 exec SpListarGastosAcumulados 1996, 1
 go
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- Pregunta 01
+
+-- max(select count(*) from Products pro on od.ProductID) as 'Menos vendido'
+
+
+select top 5
+od.ProductID, 
+p.ProductName, 
+od.Quantity,
+od.OrderID,
+count(p.UnitPrice * od.Quantity * (1 - Discount))
+-- (select sum(UnitPrice * Quantity * (1 - Discount)) from OrderDetails od where od.OrderID = o.OrderID) as 'Valor Orden',
+-- min(od.Quantity) as 'Menos vendido'
+from OrderDetails od
+inner join Products p on p.ProductID = od.ProductID
+inner join Suppliers s on s.SupplierID = p.SupplierID
+where s.Country in ('Japan', 'Finland')
+group by od.ProductID, p.ProductName, od.Quantity, od.OrderID
+go
+
+-- # Vista con SELECT de SELECT
+--create view vTotalOrdenes as
+--select 
+--o.OrderID,
+--o.OrderDate,
+--(select sum(UnitPrice * Quantity * (1 - Discount)) from OrderDetails od where od.OrderID = o.OrderID) as 'Valor Orden',
+--(select count(*) from OrderDetails od where od.OrderID = o.OrderID) as 'Cantidad de Productos',
+--c.CompanyName,e.LastName, o.ShipCountry, o.ShipCity
+--from Orders o
+--join Customers c on c.CustomerID = o.CustomerID
+--join Employees e on e.EmployeeID = o.EmployeeID
+
+
+
+-- Pregunta 02
+
+create or alter function dbo.FnNombreApellido
+(
+	@ProductID int
+) returns varchar (250)
+as
+begin
+	declare @nombrecompleto varchar(250)
+
+	select @nombrecompleto = e.FirstName + ' '+ e.LastName 
+	from Orders o 
+	join OrderDetails od on od.OrderID = o.OrderID
+	join Employees e on e.EmployeeID = o.EmployeeID
+	join Products p on p.ProductID = od.ProductID
+	where od.ProductID = @ProductID
+	group by e.FirstName ,e.LastName ,od.Quantity
+	having od.Quantity <= max(od.Quantity)
+
+	return @nombrecompleto
+end
+go
+
+select dbo.FnNombreApellido(1) as 'Nombre completo empleado'
+
+
+-- Pregunta 03
+select 
+e.FirstName,
+count(o.OrderID)
+from 
+Orders o
+join Employees e on  e.EmployeeID = o.EmployeeID
+where year(o.ShippedDate) = '1996' 
+-- Pregunta 04
+
+Preparar una consulta que permita obtener la suma y el promedio del valor de las ordenes (precio unitario, cantidad y descuento) por cliente para aquellas ordenes que fueron registradas en el año 1998 utilizando la cláusula OVER. Se deberá mostrar el nombre del cliente evitando los registros repetidos.
+
+
+Preparar una consulta que permita listar todos los empleados que tengan más de 20 órdenes registradas en el año 1996.
+
+
+Pregunta 26 pts
+Crear una función que reciba el Id del Producto y devuelva el nombre y el apellido del empleado que recibió la orden con la mayor cantidad del producto indicado.
+
+Realizar una consulta que muestre los primeros 5 productos menos vendidos de los proveedores cuyos países son de Japón y Finlandia. Se debe mostrar el ID del Producto, la Descripción y la Cantidad.
