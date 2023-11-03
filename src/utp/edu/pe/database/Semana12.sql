a0281ea5281c3b5eb5af49c6a02d0c602f52b96e
Alex Christian <alexchristianqr@gmail.com>
2023-11-02 19:29:45 -0500

    Create Semana12.sql

---
+++ src/utp/edu/pe/database/Semana12.sql
@@ -0,0 +1,39 @@
+Biblioteca
+----------
+
+Prestamos:
+----------
+- CodigoSocio
+- CodigoLibro
+- DiasPrestados
+- FechaPrestamo
+- FechaEsperadaDevolucion
+- FechaRealDevolucion
+
+Socio:
+------
+- Codigo:
+- Nombre: 
+- Apellido:
+- Telefono:
+
+Libro:
+------
+- Codigo
+- ISBN
+- CodigoAutor
+- CodigoTipoAutor
+- TotalPaginas
+- FechaNacimiento
+- FechaMuerte
+
+Autor:
+------
+- Codigo
+- Nombre
+- Apellido
+
+TipoAutor:
+----------
+- Codigo
+- Descripcion
