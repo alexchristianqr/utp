a23dd6d316821cc34842741aa0125a1bb2dfa8e0
Alex Christian <alexchristianqr@gmail.com>
2023-11-15 18:02:29 -0500

    Update Semana14.java

--- src/utp/edu/pe/poo/Semana14/Semana14.java
+++ src/utp/edu/pe/poo/Semana14/Semana14.java
@@ -34,3 +34,32 @@ class Conexion {
     
 
 }
+
+
+
+
+
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Principal {
+/** Clase principal y método MAIN */
+    public static void main(String[] args) {
+        String consulta="SELECT title,length FROM film";
+        
+        try {
+            Statement sentencia=Conexion.obtener().createStatement();
+            ResultSet resultado=sentencia.executeQuery(consulta);
+            while (resultado.next())
+            {
+                System.out.println (resultado.getString (1) + " " + resultado.getInt(2));
+            }
+            
+        } catch (ClassNotFoundException | SQLException e) {
+            
+            e.printStackTrace();
+        }
+    }
+
+}
