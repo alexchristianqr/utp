2b672c10432a0ed3df2e08481dfe5e31044238c1
Alex Christian <alexchristianqr@gmail.com>
2023-11-15 17:56:11 -0500

    Update Semana14.java

--- src/utp/edu/pe/poo/Semana14/Semana14.java
+++ src/utp/edu/pe/poo/Semana14/Semana14.java
@@ -1,1 +1,36 @@
-test
+package semana14;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+class Conexion {
+
+    private static Connection cnx = null;
+
+    public static Connection obtener() throws SQLException, ClassNotFoundException {
+        if (cnx == null) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                String xuser = "root";
+                String xpassword = "";
+                cnx = DriverManager.getConnection("jdbc:mysql://localhost:3308/inventario", xuser, xpassword);
+            } catch (SQLException ex) {
+                throw new SQLException(ex);
+            } catch (ClassNotFoundException ex) {
+                throw new ClassCastException(ex.getMessage());
+            }
+        }
+        return cnx;
+    }
+
+    public static void cerrar() throws SQLException {
+        if (cnx != null) {
+            cnx.close();
+        }
+    }
+    
+    
+
+}
