d4a4559923a6da5cb8d9c6d58d41110cfbd7de5d
Alex Christian <alexchristianqr@gmail.com>
2023-12-02 22:55:50 -0500

    [updated]

--- src/utp/edu/pe/database/proyecto-final.sql
+++ src/utp/edu/pe/database/proyecto-final.sql
@@ -153,30 +153,30 @@ create table ComprobantePago
 )
 
 -- Perfil
-insert into Perfil (PerfilID, Nombre, Permisos, FechaCreado) values (1, 'Administrador', '{}', '2023-11-10 18:45:29')
-insert into Perfil (PerfilID, Nombre, Permisos, FechaCreado) values (2, 'Recepcionista', '{}', '2023-11-09 10:23:51')
-insert into Perfil (PerfilID, Nombre, Permisos, FechaCreado) values (3, 'Portero', '{}', '2023-11-08 08:51:49')
-insert into Perfil (PerfilID, Nombre, Permisos, FechaCreado) values (4, 'Conserje', '{}', '2023-11-07 11:01:36')
-insert into Perfil (PerfilID, Nombre, Permisos, FechaCreado) values (5, 'Personal de limpieza', '{}', '2023-11-06 16:06:02')
-insert into Perfil (PerfilID, Nombre, Permisos, FechaCreado) values (6, 'Personal de mantenimiento', '{}', '2023-11-05 12:01:20')
+insert into Perfil (Nombre, Permisos, FechaCreado) values ('Administrador', '{}', '2023-11-10 18:45:29')
+insert into Perfil (Nombre, Permisos, FechaCreado) values ('Recepcionista', '{}', '2023-11-09 10:23:51')
+insert into Perfil (Nombre, Permisos, FechaCreado) values ('Portero', '{}', '2023-11-08 08:51:49')
+insert into Perfil (Nombre, Permisos, FechaCreado) values ('Conserje', '{}', '2023-11-07 11:01:36')
+insert into Perfil (Nombre, Permisos, FechaCreado) values ('Personal de limpieza', '{}', '2023-11-06 16:06:02')
+insert into Perfil (Nombre, Permisos, FechaCreado) values ('Personal de mantenimiento', '{}', '2023-11-05 12:01:20')
 
 -- Persona Empleado
-insert into Persona (PersonaID, Nombre, Apellido, TipoDocumento, NroDocumento, Sexo, Edad, FechaCreado) values (1, 'Luis', 'Torres', 1, '12345678', 'M', '30', '2023-11-10 18:45:29')
-insert into Empleado (EmpleadoID, PersonaID, PerfilID, Sueldo, FechaCreado) values (1, 1, 1, 1250.69, '2023-11-10 18:45:29')
+insert into Persona (Nombre, Apellido, TipoDocumento, NroDocumento, Sexo, Edad, FechaCreado) values ('Luis', 'Torres', 1, '12345678', 'M', '30', '2023-11-10 18:45:29')
+insert into Empleado (PersonaID, PerfilID, Sueldo, FechaCreado) values (1, 1, 1250.69, '2023-11-10 18:45:29')
 
 -- Persona Cliente
-insert into Persona (PersonaID, Nombre, Apellido, TipoDocumento, NroDocumento, Sexo, Edad, FechaCreado) values (2, 'Javier', 'Retamoso', 1, '74567890', 'M', '30', '2023-11-10 18:45:29')
-insert into Cliente (ClienteID, PersonaID, Empresa, FechaCreado) values (1, 2, null, '2023-11-10 18:45:29')
+insert into Persona (Nombre, Apellido, TipoDocumento, NroDocumento, Sexo, Edad, FechaCreado) values ('Javier', 'Retamoso', 1, '74567890', 'M', '30', '2023-11-10 18:45:29')
+insert into Cliente (PersonaID, Empresa, FechaCreado) values (2, null, '2023-11-10 18:45:29')
 
 -- Tipo Habitacion
-insert into TipoHabitacion (TipoHabitacionID, Descripcion, FechaCreado) values (1, 'Clasico', '2023-11-10 18:45:29')
+insert into TipoHabitacion (Descripcion, FechaCreado) values ('Clasico', '2023-11-10 18:45:29')
 
 -- Habitacion
-insert into Habitacion (HabitacionID, TipoHabitacionID, Descripcion, Nivel, NumeroPiso, Precio, CantidadCamas, FechaCreado) values (1, 1, 'Habitación con agua caliente + TV', '1', '101', 49.50, 1, '2023-11-10 18:45:29')
+insert into Habitacion (TipoHabitacionID, Descripcion, Nivel, NumeroPiso, Precio, CantidadCamas, FechaCreado) values (1, 1, 'Habitación con agua caliente + TV', '1', '101', 49.50, 1, '2023-11-10 18:45:29')
 
 -- Reserva
-insert into Reserva (ReservaID, ClienteID, HabitacionID, MontoTotal, FechaReserva, FechaEntrada, FechaSalida, FechaCreado)
-  values (1, 1, 1, 89.90, '2023-11-10 18:45:29', '2023-11-10 18:45:29', '2023-11-10 18:45:29', '2023-11-10 18:45:29')
+insert into Reserva (ClienteID, HabitacionID, MontoTotal, FechaReserva, FechaEntrada, FechaSalida, FechaCreado)
+  values (1, 1, 89.90, '2023-11-10 18:45:29', '2023-11-10 18:45:29', '2023-11-10 18:45:29', '2023-11-10 18:45:29')
 
 -- Producto
 insert into Producto (ProductoID, Descripcion, Precio, CantidadStock, FechaCreado) values (1, 'Botella de agua cielo', 5, 100, '2023-11-10 18:45:29')
