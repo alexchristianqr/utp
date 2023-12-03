edbef96998950d615cd774656100e92a56993d53
Alex Christian <alexchristianqr@gmail.com>
2023-12-03 00:26:36 -0500

    [updated]

--- src/utp/edu/pe/database/dbhotel_DATA.sql
+++ src/utp/edu/pe/database/dbhotel_DATA.sql
@@ -1,23 +1,24 @@
 -- Perfil
-insert into Perfil (Nombre, Permisos, FechaCreado) values ('Administrador', '{}', '2023-11-10 18:45:29')
--- Persona Empleado
-insert into Persona (Nombre, Apellido, TipoDocumento, NroDocumento, Sexo, Edad, FechaCreado) values ('Luis', 'Torres', '1', '12345678', 'M', '30', '2023-11-10 18:45:29')
-insert into Empleado (PersonaID, PerfilID, Sueldo, FechaCreado) values (1, 1, 1250.69, '2023-11-10 18:45:29')
--- Persona Cliente
-insert into Persona (Nombre, Apellido, TipoDocumento, NroDocumento, Sexo, Edad, FechaCreado) values ('Javier', 'Retamoso', '1', '74567890', 'M', '30', '2023-11-10 18:45:29')
-insert into Cliente (PersonaID, Empresa, FechaCreado) values (2, null, '2023-11-10 18:45:29')
+insert into Perfil (Nombre, Permisos, FechaCreado) values ('Administrador', '{}',  GETDATE())
+insert into Perfil (Nombre, Permisos, FechaCreado) values ('Recepcionista', '{}', GETDATE())
+-- Persona Empleado (IMPARES)
+insert into Persona (Nombre, Apellido, TipoDocumento, NroDocumento, Sexo, Edad, FechaCreado) values ('Luis', 'Torres', '1', '12345678', 'M', '30',  GETDATE())
+insert into Empleado (PersonaID, PerfilID, Sueldo, FechaCreado) values (SCOPE_IDENTITY(), 1, 1250.69,  GETDATE())
+-- Persona Cliente (PARES)
+insert into Persona (Nombre, Apellido, TipoDocumento, NroDocumento, Sexo, Edad, FechaCreado) values ('Javier', 'Retamoso', '1', '74567890', 'M', '30',  GETDATE())
+insert into Cliente (PersonaID, Empresa, FechaCreado) values (SCOPE_IDENTITY(), null,  GETDATE())
 -- Tipo Habitacion
-insert into TipoHabitacion (Descripcion, FechaCreado) values ('Clasico', '2023-11-10 18:45:29')
+insert into TipoHabitacion (Descripcion, FechaCreado) values ('Clasico',  GETDATE())
 -- Habitacion
-insert into Habitacion (TipoHabitacionID, Descripcion, Nivel, NumeroPiso, Precio, CantidadCamas, FechaCreado) values (1, 'Habitación con agua caliente + TV', '1', '101', 49.50, 1, '2023-11-10 18:45:29')
+insert into Habitacion (TipoHabitacionID, Descripcion, Nivel, NumeroPiso, Precio, CantidadCamas, FechaCreado) values (1, 'Habitación con agua caliente + TV', '1', '101', 49.50, 1,  GETDATE())
 -- Reserva
-insert into Reserva (ClienteID, HabitacionID, EmpleadoID, MontoTotal, CantidadPersonas, Estado, FechaReserva, FechaEntrada, FechaSalida, FechaCreado) values (1, 1, 1, 89.90, 2, 'activo', '2023-11-10 18:45:29', '2023-11-10 18:45:29', '2023-11-10 18:45:29', '2023-11-10 18:45:29')
+insert into Reserva (ClienteID, HabitacionID, EmpleadoID, MontoTotal, CantidadPersonas, Estado, FechaReserva, FechaEntrada, FechaSalida, FechaCreado) values (1, 1, 1, 89.90, 2, 'activo',  GETDATE(),  GETDATE(),  GETDATE(),  GETDATE())
 -- Producto
-insert into Producto (Descripcion, Precio, CantidadStock, FechaCreado) values ('Botella de agua cielo', 5, 100, '2023-11-10 18:45:29')
+insert into Producto (Descripcion, Precio, CantidadStock, FechaCreado) values ('Botella de agua cielo', 5, 100,  GETDATE())
 -- Reserva Consumo
-insert into ReservaConsumo (ReservaID, ProductoID, Cantidad, Precio, FechaCreado) values (1, 1, 3, 5.99, '2023-11-10 18:45:29')
+insert into ReservaConsumo (ReservaID, ProductoID, Cantidad, Precio, FechaCreado) values (1, 1, 3, 5.99,  GETDATE())
 -- Comprobante Pago
-insert into ComprobantePago (ReservaID, EmpleadoID, TipoComprobante, FechaCreado, FechaPagado, Estado) values (1, 1, '1', '2023-11-10 18:45:29', '2023-11-10 18:45:29', 'Pagado')
+insert into ComprobantePago (ReservaID, EmpleadoID, TipoComprobante, FechaCreado, FechaPagado, Estado) values (1, 1, '1',  GETDATE(),  GETDATE(), 'Pagado')
 
 -- * --
 
