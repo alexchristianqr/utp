3739b632cb73310e78042cf091ad6f07f852eea1
Alex Christian <alexchristianqr@gmail.com>
2024-08-14 18:59:39 -0500

    Update semana01.pl

--- src/utp/edu/programacion-logica-funcional/semana01.pl
+++ src/utp/edu/programacion-logica-funcional/semana01.pl
@@ -1,1 +1,12 @@
-
+% Definimos algunos hechos simples
+perro(bruno).
+perro(jon).
+gato(tom).
+gato(felix).
+pajaro(lia).
+pajaro(urraco).
+pajaro(juan).
+% Consulta simple
+animal(X) :- gato(X).
+animal(X) :- perro(X).
+animal(X) :- pajaro(X).
