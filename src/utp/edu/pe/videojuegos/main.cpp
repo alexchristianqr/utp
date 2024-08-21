db1daf2caec02330cea99d834aa8d1ca39948bf5
Alex Christian <alexchristianqr@gmail.com>
2024-08-20 21:34:10 -0500

    Create main.cpp

---
+++ src/utp/edu/pe/videojuegos/main.cpp
@@ -0,0 +1,115 @@
+//---------------------------------------------------------------------------
+// Programming 3D to Newbies : Indiana Aiki
+//---------------------------------------------------------------------------
+
+//#include "L02_Environment.h"
+//          L02_Environment scene;
+
+//#include "L03_Background.h"
+//          L03_Background scene;
+
+//#include "L04_Vars.h"
+//          L04_Vars scene;
+
+//#include "L05_Click.h"
+//          L05_Click scene;
+
+//#include "L06_IF.h"
+//          L06_IF scene;
+
+//#include "L07_Triangle.h"
+//          L07_Triangle scene;
+
+//#include "L08_Procedures.h"
+//          L08_Procedures scene;
+
+//#include "L09_Color.h"
+//          L09_Color scene;
+
+//#include "L10_Rotate.h"
+//          L10_Rotate scene;
+
+//#include "L11_RotateClick.h"
+//          L11_RotateClick scene;
+		  
+//#include "L12_Animation.h"
+//          L12_Animation scene;
+
+//#include "L13_Square.h"
+//          L13_Square scene;
+
+//#include "L14_While.h"
+//          L14_While scene;
+
+//#include "L15_Polygon.h"
+//          L15_Polygon scene;
+
+//#include "L16_PolygonClick.h"
+//          L16_PolygonClick scene;
+
+//#include "L17_PolygonAnimation.h"
+//          L17_PolygonAnimation scene;
+
+//#include "L18_Points.h"
+//          L18_Points scene;
+
+//#include "L19_Lines.h"
+//          L19_Lines scene;
+          
+//#include "L20_LocalVar.h"
+//          L20_LocalVar scene;
+
+//#include "L21_PointsAnimation.h"
+//          L21_PointsAnimation scene;
+          
+//#include "L22_LinesCube.h"
+//          L22_LinesCube scene;
+          
+//#include "L23_Camera.h"
+//          L23_Camera scene;
+
+//#include "L24_Curve.h"
+//          L24_Curve scene;
+
+//#include "L25_Circles.h"
+//          L25_Circles scene;
+
+//#include "L26_Pyramid.h"
+//          L26_Pyramid scene;
+
+//#include "L27_LoopCounter.h"
+//          L27_LoopCounter scene;
+
+//#include "L28_Translate.h"
+//          L28_Translate scene;
+
+//#include "L29_RotateTranslate.h"
+//          L29_RotateTranslate scene;
+
+//#include "L30_Scale.h"
+//          L30_Scale scene;
+
+//#include "L31_CubeFaces.h"
+//          L31_CubeFaces scene;
+
+//#include "L32_CubeAnimation.h"
+//          L32_CubeAnimation scene;
+
+#include "Main_Glut.h"
+
+int main ( )
+{
+   //main_WinPosX   = 820;
+   //main_WinPosY   = 100;
+   //main_WinWidth  = 450;
+   //main_WinHeight = 350;
+
+   main_WinPosX   = 656;
+   main_WinPosY   = 100;
+   main_WinWidth  = 360;
+   main_WinHeight = 275;
+
+   main_Glut();
+
+   return 0;
+}
