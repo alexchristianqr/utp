da1e9e7697c429eb09e62023ff161506407b0c09
Alex Christian <alexchristianqr@gmail.com>
2024-09-04 19:43:43 -0500

    Update tarea_s4.pl

--- src/utp/edu/pe/programacion-logica-funcional/tarea_s4.pl
+++ src/utp/edu/pe/programacion-logica-funcional/tarea_s4.pl
@@ -1,4 +1,4 @@
-# Hechos aviones(codigo,marca,capacidad)
+% Hechos aviones(codigo,marca,capacidad)
 aviones(1,latam,200).
 
-# vuelo(avion, pasajeros, salida, llegada, horario)
+% vuelo(avion, pasajeros, salida, llegada, horario)
