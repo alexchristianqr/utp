942ac8bc5a842a13b05ea026bd0eb385374f9310
Alex Christian <alexchristianqr@gmail.com>
2024-09-04 19:55:05 -0500

    Update tarea_s4.pl

--- src/utp/edu/pe/programacion-logica-funcional/tarea_s4.pl
+++ src/utp/edu/pe/programacion-logica-funcional/tarea_s4.pl
@@ -1,4 +1,13 @@
-% Hechos aviones(codigo,marca,capacidad)
+% Hechos aviones(codigo, aerolinea, capacidad)
 aviones(1,latam,200).
+aviones(2, avianca, 180).
+aviones(3, aeromexico, 220).
+aviones(4, copa, 150).
+aviones(5, gol, 170).
+aviones(6, azul, 140).
+aviones(7, sky, 190).
+aviones(8, aerolineas_argentinas, 210).
+aviones(9, viva_air, 160).
+aviones(10, interjet, 230).
 
 % vuelo(avion, pasajeros, salida, llegada, horario)
