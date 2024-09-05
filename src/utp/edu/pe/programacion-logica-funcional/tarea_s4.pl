1c71bd28caa4421961784d7c29e825d455e6e063
Sebastián Reyes <71537939+sebastian-reyes@users.noreply.github.com>
2024-09-04 19:50:22 -0500

    Update tarea_s4.pl

--- src/utp/edu/pe/programacion-logica-funcional/tarea_s4.pl
+++ src/utp/edu/pe/programacion-logica-funcional/tarea_s4.pl
@@ -1,4 +1,5 @@
 % Hechos aviones(codigo,marca,capacidad)
 aviones(1,latam,200).
 
-% vuelo(avion, pasajeros, salida, llegada, horario)
+% vuelo(cod_vuelo, tipo_vuelo, avion, piloto, pasajeros, salida, llegada, fecha, hora)
+vuelo(1, nacional, 1, 1, [juan_perez, ruth_torres, alex_quispe, mario_vargas, jose_reyes, sebastian_mogollon], lima, cuzco, '10-05-24', '15:00')
