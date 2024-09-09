2d7e2353469a2f1f5468eed907b56e70fea10c98
Sebastián Reyes <71537939+sebastian-reyes@users.noreply.github.com>
2024-09-09 01:50:32 -0500

    Agregando reglas para vuelos en tarea_s4.pl

--- src/utp/edu/pe/programacion-logica-funcional/tarea_s4.pl
+++ src/utp/edu/pe/programacion-logica-funcional/tarea_s4.pl
@@ -50,34 +50,76 @@ pilotos_experimentados(Resultado) :-
      findall(Codigo, (pilotos(Codigo,_,_,_,AniosExp), AniosExp >= 10), Resultado).
 % ?- pilotos_experimentados(Resultado).
 
-% Hechos vuelo(cod_vuelo, tipo_vuelo, avion, piloto, pasajeros, salida, llegada, fecha, hora)
+% Hechos Vuelo(cod_vuelo, tipo_vuelo, avion, piloto, pasajeros, salida, llegada, fecha, hora)
 vuelo(1, nacional, 1, 1, [juan_perez, ruth_torres, alex_quispe, mario_vargas, jose_reyes, sebastian_mogollon], lima, cuzco, '10-05-24', '15:00').
-vuelo(2, internacional, 2, 2, [marta_lopez, fernando_garcia, lucia_fernandez], madrid, paris, '11-05-24', '09:30').
-vuelo(3, nacional, 3, 3, [carmen_diaz, eduardo_martinez, laura_castro], bogota, medellin, '12-05-24', '12:45').
-vuelo(4, nacional, 4, 4, [pedro_sanabria, maria_ramos], quito, guayaquil, '13-05-24', '16:00').
-vuelo(5, internacional, 5, 5, [ana_gonzalez, joseph_davis], new_york, london, '14-05-24', '22:15').
-vuelo(6, nacional, 6, 6, [daniela_morales, felipe_herrera], santiago, valparaiso, '15-05-24', '08:30').
-vuelo(7, internacional, 7, 7, [olga_ruiz, marcos_garcia], sao_paulo, buenos_aires, '16-05-24', '19:00').
-vuelo(8, nacional, 8, 8, [mario_flores, carla_moreno], montevideo, asuncion, '17-05-24', '14:20').
-vuelo(9, internacional, 9, 9, [luisa_castano, fernando_jimenez], rio_de_janeiro, miami, '18-05-24', '23:10').
-vuelo(10, nacional, 10, 10, [carlos_fernandez, sofia_rodriguez], medellin, bogota, '19-05-24', '11:00').
-vuelo(11, nacional, 11, 11, [jesus_morales, estela_gutierrez], san_pedro_sula, tegucigalpa, '20-05-24', '06:50').
-vuelo(12, internacional, 12, 12, [beatriz_mendez, juan_valencia], los_angeles, vancouver, '21-05-24', '17:40').
-vuelo(13, nacional, 13, 13, [paola_escobar, ricardo_ponce], caracas, maracaibo, '22-05-24', '21:30').
-vuelo(14, internacional, 14, 14, [jose_moreno, carla_silva], madrid, rome, '23-05-24', '13:15').
-vuelo(15, nacional, 15, 15, [jose_antonio, valeria_beltran], lima, arequipa, '24-05-24', '18:00').
-vuelo(16, nacional, 16, 16, [maria_sanchez, eduardo_diaz], santiago, concepcion, '25-05-24', '10:05').
-vuelo(17, internacional, 17, 17, [diego_hernandez, laura_rivera], new_york, san_paulo, '26-05-24', '12:25').
-vuelo(18, nacional, 18, 18, [martha_perez, antonio_rios], lima, piura, '27-05-24', '14:10').
-vuelo(19, internacional, 19, 19, [carmen_bermudez, manuel_zapata], berlin, zurich, '28-05-24', '16:00').
-vuelo(20, nacional, 20, 20, [andres_uribe, daniela_aro], medellin, cartagena, '29-05-24', '11:30').
-vuelo(21, internacional, 21, 21, [sandra_jimenez, antonio_uribe], sydney, auckland, '30-05-24', '19:45').
-vuelo(22, nacional, 22, 22, [ivan_ponce, juliana_carreno], quito, cuenca, '31-05-24', '08:00').
-vuelo(23, internacional, 23, 23, [veronica_moreno, alberto_perez], tokyo, beijing, '01-06-24', '23:00').
-vuelo(24, nacional, 24, 24, [camila_mora, gustavo_gallegos], buenos_aires, cordoba, '02-06-24', '17:30').
-vuelo(25, internacional, 25, 25, [karen_lopez, andres_garcia], miami, buenos_aires, '03-06-24', '10:10').
-vuelo(26, nacional, 26, 26, [gabriel_rivera, lucia_diaz], bogota, barranquilla, '04-06-24', '15:55').
-vuelo(27, internacional, 27, 27, [julio_paredes, estefania_silva], new_york, madrid, '05-06-24', '14:00').
-vuelo(28, nacional, 28, 28, [alicia_jimenez, samuel_mosquera], quito, guayaquil, '06-06-24', '12:20').
-vuelo(29, internacional, 29, 29, [mario_castro, isabella_torres], london, tokyo, '07-06-24', '21:10').
-vuelo(30, nacional, 30, 30, [lorena_vega, miguel_cardenas], arequipa, cusco, '08-06-24', '09:30').
+vuelo(2, internacional, 2, 2, [marta_lopez, fernando_garcia, lucia_fernandez, laura_rodriguez, oscar_lopez], madrid, paris, '11-05-24', '09:30').
+vuelo(3, nacional, 3, 3, [carmen_diaz, eduardo_martinez, laura_castro, juan_gomez, maria_suarez], bogota, medellin, '12-05-24', '12:45').
+vuelo(4, nacional, 4, 4, [pedro_sanabria, maria_ramos, carlos_perez, luisa_sanchez, victor_morales], quito, guayaquil, '13-05-24', '16:00').
+vuelo(5, internacional, 5, 5, [ana_gonzalez, joseph_davis, carlos_torres, sandra_velez, mario_flores], new_york, london, '14-05-24', '22:15').
+vuelo(6, nacional, 6, 6, [daniela_morales, felipe_herrera, sofia_fernandez, alberto_ramos, juan_vega], santiago, valparaiso, '15-05-24', '08:30').
+vuelo(7, internacional, 7, 7, [olga_ruiz, marcos_garcia, beatriz_torres, ricardo_diaz, esteban_martinez], sao_paulo, buenos_aires, '16-05-24', '19:00').
+vuelo(8, nacional, 8, 8, [mario_flores, carla_moreno, luis_gomez, andrea_rodriguez, juan_castillo], montevideo, asuncion, '17-05-24', '14:20').
+vuelo(9, internacional, 9, 9, [luisa_castano,juan_perez , alicia_vega, pablo_mora, clara_torres], rio_de_janeiro, miami, '18-05-24', '23:10').
+vuelo(10, nacional, 10, 10, [carlos_fernandez, sofia_rodriguez, diana_vargas, jose_rivera, manuel_diaz], medellin, bogota, '19-05-24', '11:00').
+vuelo(11, nacional, 11, 1, [jesus_morales, estela_gutierrez, patricia_gonzalez, pablo_garcia, jorge_lopez], san_pedro_sula, tegucigalpa, '20-05-24', '06:50').
+vuelo(12, internacional, 12, 2, [beatriz_mendez, juan_valencia, maria_arias, jose_garcia, andrea_flores], los_angeles, vancouver, '21-05-24', '17:40').
+vuelo(13, nacional, 13, 3, [paola_escobar, ricardo_ponce, victor_suarez, andres_fernandez, ana_martinez], caracas, maracaibo, '22-05-24', '21:30').
+vuelo(14, internacional, 14, 4, [jose_moreno, carla_silva, marcos_torres, laura_diaz, juan_fernandez], madrid, rome, '23-05-24', '13:15').
+vuelo(15, nacional, 15, 5, [jose_antonio, valeria_beltran, carlos_flores, daniela_gomez, patricia_rojas], lima, arequipa, '24-05-24', '18:00').
+vuelo(16, nacional, 16, 6, [maria_sanchez, eduardo_diaz, diana_moreno, felipe_martinez, mariana_lopez], santiago, concepcion, '25-05-24', '10:05').
+vuelo(17, internacional, 17, 7, [diego_hernandez, laura_rivera, pablo_morales, andrea_garcia, sofia_torres], new_york, sao_paulo, '26-05-24', '12:25').
+vuelo(18, nacional, 18, 8, [martha_perez, antonio_rios, carlos_vega, paula_diaz, jorge_morales], lima, piura, '27-05-24', '14:10').
+vuelo(19, internacional, 19, 9, [carmen_bermudez, manuel_zapata, andrea_moreno, jose_perez, sandra_suarez], berlin, zurich, '28-05-24', '16:00').
+vuelo(20, nacional, 20, 10, [andres_uribe, daniela_aro, juan_suarez, ana_martinez, carlos_torres], medellin, cartagena, '29-05-24', '11:30').
+vuelo(21, internacional, 1, 1, [sandra_jimenez, antonio_uribe, pedro_gomez, luisa_rojas, carlos_perez], sydney, auckland, '30-05-24', '19:45').
+vuelo(22, nacional, 2, 2, [ivan_ponce, juliana_carreno, maria_torres, juan_lopez, patricia_moreno], quito, cuenca, '31-05-24', '08:00').
+vuelo(23, internacional, 3, 3, [veronica_moreno, alberto_perez, sofia_diaz, jose_ramos, maria_gonzalez], tokyo, beijing, '01-06-24', '23:00').
+vuelo(24, nacional, 4, 4, [camila_mora, gustavo_gallegos, juan_velez, paola_ramos, victor_torres], buenos_aires, cordoba, '02-06-24', '17:30').
+vuelo(25, internacional, 5, 5, [karen_lopez, andres_garcia, mario_rodriguez, juan_felipe, luisa_perez], miami, buenos_aires, '03-06-24', '10:10').
+vuelo(26, nacional, 6, 6, [gabriel_rivera, lucia_diaz, sofia_garcia, carlos_fernandez, andrea_perez], bogota, barranquilla, '04-06-24', '15:55').
+vuelo(27, internacional, 7, 7, [julio_paredes, estefania_silva, juan_lopez, maria_torres, jose_garcia], new_york, madrid, '05-06-24', '14:00').
+vuelo(28, nacional, 8, 8, [alicia_jimenez, samuel_mosquera, luisa_flores, mario_perez, jorge_martinez], quito, guayaquil, '06-06-24', '12:20').
+vuelo(29, internacional, 9, 9, [mario_castro, isabella_torres, pablo_gomez, carla_moreno, jose_ramirez], london, tokyo, '07-06-24', '21:10').
+vuelo(30, nacional, 10, 10, [lorena_vega, miguel_cardenas, juan_diaz, sofia_morales, eduardo_lopez], arequipa, cusco, '08-06-24', '09:30').
+vuelo(31, internacional, 11, 1, [sandra_jimenez, antonio_uribe, pedro_gomez, luisa_rojas, carlos_perez], sydney, auckland, '09-06-24', '19:45').
+vuelo(32, nacional, 12, 2, [ivan_ponce, juliana_carreno, maria_torres, juan_lopez, patricia_moreno], quito, cuenca, '10-06-24', '08:00').
+vuelo(33, internacional, 13, 3, [veronica_moreno, alberto_perez, sofia_diaz, jose_ramos, maria_gonzalez], tokyo, beijing, '11-06-24', '23:00').
+vuelo(34, nacional, 14, 4, [camila_mora, gustavo_gallegos, juan_velez, paola_ramos, victor_torres], buenos_aires, cordoba, '12-06-24', '17:30').
+vuelo(35, internacional, 15, 5, [karen_lopez, andres_garcia, mario_rodriguez, juan_felipe, luisa_perez], miami, buenos_aires, '13-06-24', '10:10').
+vuelo(36, nacional, 16, 6, [gabriel_rivera, lucia_diaz, sofia_garcia, carlos_fernandez, andrea_perez], bogota, barranquilla, '14-06-24', '15:55').
+vuelo(37, internacional, 17, 7, [julio_paredes, estefania_silva, juan_lopez, maria_torres, jose_garcia], new_york, madrid, '15-06-24', '14:00').
+vuelo(38, nacional, 18, 8, [alicia_jimenez, samuel_mosquera, luisa_flores, mario_perez, jorge_martinez], quito, guayaquil, '16-06-24', '12:20').
+vuelo(39, internacional, 19, 9, [mario_castro, isabella_torres, pablo_gomez, carla_moreno, jose_ramirez], london, tokyo, '17-06-24', '21:10').
+vuelo(40, nacional, 20, 10, [lorena_vega, miguel_cardenas, juan_diaz, juan_perez ,eduardo_lopez], arequipa, cusco, '18-06-24', '09:30').
+vuelo(41, internacional, 1, 1, [alexa_morales, hugo_perez, daniela_mendez, carlos_vega, laura_moreno], sydney, auckland, '19-06-24', '19:45').
+vuelo(42, nacional, 2, 2, [gabriel_rios, mariana_torres, juan_perez, paola_sanchez, sofia_garcia], quito, cuenca, '20-06-24', '08:00').
+vuelo(43, internacional, 3, 3, [veronica_ramirez, alberto_fernandez, sofia_velez, jose_garcia, maria_sanchez], tokyo, beijing, '21-06-24', '23:00').
+vuelo(44, nacional, 4, 4, [camila_rivera, gustavo_mora, juan_velez, paola_castano, victor_morales], buenos_aires, cordoba, '22-06-24', '17:30').
+vuelo(45, internacional, 5, 5, [karen_paredes, andres_moreno, mario_rojas, juan_fernandez, luisa_torres], miami, buenos_aires, '23-06-24', '10:10').
+vuelo(46, nacional, 6, 6, [gabriel_lopez, lucia_perez, sofia_mora, juan_perez, andrea_vega], bogota, barranquilla, '24-06-24', '15:55').
+vuelo(47, internacional, 7, 7, [julio_silva, estefania_torres, juan_diaz, maria_gonzalez, jose_ramirez], new_york, madrid, '25-06-24', '14:00').
+vuelo(48, nacional, 8, 8, [alicia_garcia, samuel_perez, luisa_morales, mario_vega, jorge_rodriguez], quito, guayaquil, '26-06-24', '12:20').
+vuelo(49, internacional, 9, 9, [mario_torres, isabella_flores, pablo_sanchez, carla_velez, jose_moreno], london, tokyo, '27-06-24', '21:10').
+vuelo(50, nacional, 10, 10, [lorena_sanchez, miguel_vega, juan_castano, sofia_rih, eduardo_suarez], arequipa, cusco, '28-06-24', '09:30').
+
+% Regla: vuelos por tipo
+vuelos_por_tipo(Tipo, Resultado) :-
+    findall(ID, vuelo(ID, Tipo, _, _, _, _, _, _, _), Resultado).
+% ?- vuelos_por_tipo(internacional, Resultado), write(Resultado).
+
+% Regla: obtener los vuelos en los que ha estado un pasajero
+vuelos_por_pasajero(NombrePasajero, Resultado) :-
+    findall(ID, (vuelo(ID, _, _, _, Pasajeros, _, _, _, _), member(NombrePasajero, Pasajeros)), Resultado).
+% ?- vuelos_por_pasajero(jose_reyes, Resultado).
+
+% Regla: obtener el mejor viajero (más frecuente)
+contar_pasajero(Pasajero, Cantidad) :-
+    findall(Pasajero, (vuelo(_, _, _, _, Pasajeros, _, _, _, _), member(Pasajero, Pasajeros)), Lista),
+    length(Lista, Cantidad).
+
+mejor_viajero(Pasajero, Cantidad) :-
+    findall(Cantidad-Pasajero, (vuelo(_, _, _, _, Pasajeros, _, _, _, _), member(Pasajero, Pasajeros), contar_pasajero(Pasajero, Cantidad)), Lista),
+    sort(0, @>=, Lista, [MaxCantidad-Pasajero|_]),
+    Cantidad = MaxCantidad,
+    format('El viajero más frecuente es: ~w, con ~d apariciones.~n', [Pasajero, MaxCantidad]).
+% mejor_viajero(Pasajero, Cantidad).
